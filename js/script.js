d9be8f40a0091ff7ae5dd31e89b960bc1b131e8f
Juliano Boese <juliano.boese@gmail.com>
2022-06-11 01:13:06 -0300

    feat: :sparkles: add loading animation while fetching image suggestions

--- js/script.js
+++ js/script.js
@@ -7,6 +7,13 @@ const submitButton = document.getElementById('submit-card');
 const cardList = document.getElementById('card-list');
 
 suggestionsButton.addEventListener('click', async () => {
+  imageSuggestions.innerHTML = `
+  <svg class="animate-spin h-10 w-10" fill="none" viewBox="0 0 24 24">
+    <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
+    <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+  </svg>
+  `;
+
   const item = titleInput.value;
 
   const response = await fetch(`https://damp-beach-97624.herokuapp.com/search?item=${item}`);
