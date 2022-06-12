17a12088caaa7fb691692cf98dac50cddcc21934
JulianaNovais <juliana.novais@icloud.com>
2022-06-11 23:33:03 -0300

    continuacao de estilos e alteracao da ordem dos elementos

--- js/script.js
+++ js/script.js
@@ -1,25 +1,26 @@
-const titleInput = document.getElementById('title');
-const descriptionInput = document.getElementById('description');
-const imageInput = document.getElementById('image');
-const button = document.getElementById('submit-card');
-const cardList = document.getElementById('card-list');
+const titleInput = document.getElementById('title')
+const descriptionInput = document.getElementById('description')
+const imageInput = document.getElementById('image')
+const button = document.getElementById('submit-card')
+const cardList = document.getElementById('card-list')
 
 function addCard() {
-  const title = titleInput.value;
-  const description = descriptionInput.value;
-  const image = imageInput.value;
+  const title = titleInput.value
+  const description = descriptionInput.value
+  const image = imageInput.value
 
-  const newCard = document.createElement('li');
+  const newCard = document.createElement('li')
   newCard.innerHTML = `
+    <img src=${image} />
     <h3>${title}</h3>
     <p>${description}</p>
-    <img src=${image} />
-  `;
+    
+  `
 
-  cardList.appendChild(newCard);
+  cardList.appendChild(newCard)
 }
 
 button.addEventListener('click', (event) => {
-  event.preventDefault();
-  addCard();
-});
+  event.preventDefault()
+  addCard()
+})
